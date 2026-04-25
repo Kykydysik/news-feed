c43c8956874ff151f603ebe86209f936b5064f3b
Kykydysik <tarasowgeorgij@yandex.ru>
2026-04-25 22:24:47 +0300

    small fixes

--- frontend/app/shared/layout/CenterContent.tsx
+++ frontend/app/shared/layout/CenterContent.tsx
@@ -1,14 +1,19 @@
 import { Outlet } from 'react-router';
 import ProfileWidget from "~/modules/profile/ui/profile-widget/ProfileWidget";
+import { Toolbar } from 'primereact/toolbar';
 
 export default function CenterContent() {
   return (
-    <div className="h-screen w-screen flex flex-col items-center justify-center">
-      <div className="w-[1000px]">
-        <ProfileWidget />
+      <div className="flex flex-col gap-4">
+        <div className="card">
+          <Toolbar end={ProfileWidget} />
+        </div>
 
-        <Outlet />
+        <div className="h-screen w-screen flex flex-col items-center">
+          <div className="w-[1000px]">
+            <Outlet />
+          </div>
+        </div>
       </div>
-    </div>
   );
 }
